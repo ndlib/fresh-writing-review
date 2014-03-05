f1826e39cabffcfcde80890d2d6e548bb3917db9
Robert Fox <robertjonfox@gmail.com>
2014-03-04 19:14:34 -0500

    Add autoload path for classes in lib folder

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module FreshWriting
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.autoload_paths += %W(#{config.root}/lib)
     config.autoload_paths += Dir[Rails.root.join('lib', '{**}')]
     config.autoload_paths += Dir[Rails.root.join('app', 'view_models', '{*}')]
     config.autoload_paths += Dir[Rails.root.join('app', 'query', '{*}')]
@@ -20,6 +21,7 @@ module FreshWriting
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Eastern Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
