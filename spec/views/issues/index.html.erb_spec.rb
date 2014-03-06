3f4f09038317f1a52e8f9e53ac322c33ba150999
Jaron Kennel <jaronkk@gmail.com>
2014-03-06 12:59:11 -0500

    Added essay style list to issue show page

---
+++ spec/views/issues/index.html.erb_spec.rb
@@ -0,0 +1,14 @@
+require "spec_helper"
+
+describe "issues/index.html.erb" do
+  let(:issue_list) { IssueList.build(double()) }
+
+  before do
+    FactoryGirl.create(:issue)
+  end
+
+  it "succeeds" do
+    @issue_list = issue_list
+    render
+  end
+end
