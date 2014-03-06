257b987e7b3b5ed96cd9ff8c41b5432af527bb22
Jaron Kennel <jaronkk@gmail.com>
2014-03-06 16:30:28 -0500

    Removed unnecessary title transformation for friendly id

--- app/models/essay_style.rb
+++ app/models/essay_style.rb
@@ -1,9 +1,5 @@
 class EssayStyle < ActiveRecord::Base
   extend FriendlyId
-  friendly_id :lower_title, use: :slugged
+  friendly_id :title, use: :slugged
   store :data, accessors: [ :title ]
-
-  def lower_title
-    title.to_s.downcase
-  end
 end
--- spec/models/essay_style_spec.rb
+++ spec/models/essay_style_spec.rb
@@ -1,12 +1,18 @@
 require 'spec_helper'
 
 describe EssayStyle do
-  subject { described_class.new }
+  subject { FactoryGirl.build(:essay_style) }
 
-  [:slug, :title].each do |field|
+  [:title].each do |field|
     it "has the ##{field} attribute" do
       expect(subject).to respond_to(field)
       expect(subject).to respond_to("#{field}=")
+    end
+  end
+
+  describe '#friendly_id' do
+    it "is based on the title" do
+      expect(subject.friendly_id).to be == subject.title.downcase
     end
   end
 end
