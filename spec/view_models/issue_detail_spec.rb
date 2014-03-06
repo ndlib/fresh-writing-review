8f1293371a1105d1f70f6c8e40504368891c2260
Jaron Kennel <jaronkk@gmail.com>
2014-03-06 10:22:20 -0500

    Additional fields for issue factory and issue detail

--- spec/view_models/issue_detail_spec.rb
+++ spec/view_models/issue_detail_spec.rb
@@ -11,4 +11,22 @@ describe IssueDetail do
       expect(subject.year).to be == issue.year
     end
   end
+
+  describe '#editorial_notes' do
+    it 'is the issue notes' do
+      expect(subject.editorial_notes).to be == issue.editorial_notes
+    end
+  end
+
+  describe '#editorial_board' do
+    it 'is the issue board' do
+      expect(subject.editorial_board).to be == issue.editorial_board
+    end
+  end
+
+  describe '#acknowledgments' do
+    it 'is the issue acknowledgments' do
+      expect(subject.acknowledgments).to be == issue.acknowledgments
+    end
+  end
 end
