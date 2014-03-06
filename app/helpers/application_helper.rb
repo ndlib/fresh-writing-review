c3e1c0dbdaa1a93722631d48e609b81b8f46eecc
Jaron Kennel <jaronkk@gmail.com>
2014-03-06 15:28:26 -0500

    Application layout tweaks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def display_notices
+    content = raw("")
+    if notice
+      content += content_tag(:div, notice, class: "alert alert-info")
+    end
+    if alert
+      content += content_tag(:div, alert, class: "alert")
+    end
+    content_tag(:div, content, id: "notices")
+  end
 end
