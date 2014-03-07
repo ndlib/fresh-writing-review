d26a29453fce01e5733d0602d5761341cecf0349
Jaron Kennel <jaronkk@gmail.com>
2014-03-07 10:52:19 -0500

    Default data for 2011 and 2012 issues

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -4,7 +4,7 @@ class Issue < ActiveRecord::Base
 
   has_many :essays
 
-  store :data, accessors: [ :editorial_notes, :editorial_board, :acknowledgments ]
+  store :data, accessors: [ :editorial_notes, :editorial_board, :acknowledgments, :cover_image_fixture_path ]
 
   validates_presence_of :year
   validates_uniqueness_of :year
