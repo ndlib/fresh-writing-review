7e443627fb38ee21d460e61a222877c4c2851c9d
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2014-03-10 15:13:51 -0400

    update the seed data to be more realistic.

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,11 +15,37 @@ Issue.create(year: 2011, title: "Fresh Writing Volume 11", editorial_notes: IO.r
 
 issue_id = Issue.first.id
 Essay.delete_all
-Essay.create(id: 1, issue_id: issue_id, template: 'text', title: "1 section text", body: IO.read(Rails.root.join('spec', 'fixtures', 'narrative_text.txt')), author: "Terence Clines")
+Essay.create(id: 1, issue_id: issue_id, template: 'text', title: "Family of Deer", author: "Terence Clines",
+    body: IO.read(Rails.root.join('spec', 'fixtures', 'narrative_text.txt')),
+    author_biography: IO.read(Rails.root.join('spec', 'fixtures', 'biography.txt'))
+)
+
 Essay.create(id: 2, issue_id: issue_id, template: 'media', title: "No Transcript", body: IO.read(Rails.root.join('spec', 'fixtures', 'video_text.txt')), embed: '<iframe width="560" height="315" src="//www.youtube.com/embed/unWnZvXJH2o" frameborder="0" allowfullscreen></iframe>', author: "Terence Clines")
-Essay.create(id: 3, issue_id: issue_id, template: 'text', title: "2 section text", body: IO.read(Rails.root.join('spec', 'fixtures', 'two_section_text_part1.txt')), alt_body: IO.read(Rails.root.join('spec', 'fixtures', 'two_section_text_part2.txt')), alt_title: "Pied Beauty", author: "Terence Clines")
-Essay.create(id: 4, issue_id: issue_id, template: 'media', title: "Transcript", body: IO.read(Rails.root.join('spec', 'fixtures', 'video_text.txt')), embed: '<iframe width="560" height="315" src="//www.youtube.com/embed/unWnZvXJH2o" frameborder="0" allowfullscreen></iframe>', alt_body: "Transcript", alt_title: 'Body section title', author: "Terence Clines")
-Essay.create(id: 5, issue_id: issue_id, template: 'media', title: "The Streets Are Art", body: IO.read(Rails.root.join('spec', 'fixtures', 'zavakos_body.txt')), embed: nil, alt_body: nil, alt_title: nil, author: "Rachel Zavakos", works_cited: IO.read(Rails.root.join('spec', 'fixtures', 'zavakos_works_cited.txt')), discussion_questions: IO.read(Rails.root.join('spec', 'fixtures', 'zavakos_discussion_questions.txt')), author_biography: IO.read(Rails.root.join('spec', 'fixtures', 'biography.txt')))
+
+Essay.create(id: 3, issue_id: issue_id, template: 'text', title: "Imperfect, Contradictory Beauty",
+  body: IO.read(Rails.root.join('spec', 'fixtures', 'two_section_text_part1.txt')),
+  alt_body: IO.read(Rails.root.join('spec', 'fixtures', 'two_section_text_part2.txt')),
+  alt_title: "Pied Beauty",
+  author: "Nathan McDevitt"
+)
+
+Essay.create(id: 4, issue_id: issue_id, template: 'media', title: "Invisiball Report", author: "Chizo Ekechukwu",
+    body: IO.read(Rails.root.join('spec', 'fixtures', 'video_text.txt')),
+    embed: '<iframe width="560" height="315" src="//www.youtube.com/embed/zlMYf8MWI-4" frameborder="0" allowfullscreen></iframe>',
+    alt_body: IO.read(Rails.root.join('spec', 'fixtures', 'ekechukwu_transcript.txt')),
+    alt_title: 'Transcript'
+)
+
+
+Essay.create(id: 5, issue_id: issue_id, template: 'media', title: "The Streets Are Art", author: "Rachel Zavakos",
+    body: IO.read(Rails.root.join('spec', 'fixtures', 'zavakos_body.txt')),
+    embed: nil,
+    alt_body: nil,
+    alt_title: nil,
+    works_cited: IO.read(Rails.root.join('spec', 'fixtures', 'zavakos_works_cited.txt')),
+    discussion_questions: IO.read(Rails.root.join('spec', 'fixtures', 'zavakos_discussion_questions.txt')),
+    author_biography: IO.read(Rails.root.join('spec', 'fixtures', 'biography.txt'))
+)
 
 
 
