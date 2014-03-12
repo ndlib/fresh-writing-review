19ac7319d4566da8f9e031254cf6b7a20f3df465
Robert Fox <robertjonfox@gmail.com>
2014-03-12 09:19:27 -0400

    Set the foundation for the various join models, images and markdown content blocks

--- db/migrate/20140311162812_create_join_table_markdown_content_images.rb
+++ db/migrate/20140311162812_create_join_table_markdown_content_images.rb
@@ -1,6 +1,6 @@
-class CreateJoinTableMarkdownContentsImage < ActiveRecord::Migration
+class CreateJoinTableMarkdownContentImages < ActiveRecord::Migration
   def change
-    create_join_table :markdown_contents, :images do |t|
+    create_join_table :markdown_contents, :images, table_name: 'markdown_content_images' do |t|
       t.index [:markdown_content_id, :image_id], :name => 'md_image_index'
       t.index [:image_id, :markdown_content_id], :name => 'image_md_index'
     end
--- db/schema.rb
+++ db/schema.rb
@@ -39,6 +39,15 @@ ActiveRecord::Schema.define(version: 20140311164734) do
 
   add_index "essay_files", ["essay_id", "attached_file_id"], name: "index_essay_files_on_essay_id_and_attached_file_id", using: :btree
 
+  create_table "essay_markdown_contents", id: false, force: true do |t|
+    t.integer "essay_id",            null: false
+    t.integer "markdown_content_id", null: false
+    t.string  "title"
+  end
+
+  add_index "essay_markdown_contents", ["essay_id", "markdown_content_id"], name: "essay_md_content", using: :btree
+  add_index "essay_markdown_contents", ["markdown_content_id", "essay_id"], name: "md_content_essay", using: :btree
+
   create_table "essay_styles", force: true do |t|
     t.string   "slug"
     t.text     "data"
@@ -60,12 +69,6 @@ ActiveRecord::Schema.define(version: 20140311164734) do
   add_index "essays", ["essay_style_id"], name: "index_essays_on_essay_style_id", using: :btree
   add_index "essays", ["issue_id"], name: "index_essays_on_issue_id", using: :btree
   add_index "essays", ["slug"], name: "index_essays_on_slug", unique: true, using: :btree
-
-  create_table "essays_markdown_contents", id: false, force: true do |t|
-    t.integer "essay_id",            null: false
-    t.integer "markdown_content_id", null: false
-    t.string  "title"
-  end
 
   create_table "highlighted_essays", force: true do |t|
     t.integer  "issue_id"
@@ -89,14 +92,6 @@ ActiveRecord::Schema.define(version: 20140311164734) do
     t.datetime "updated_at"
   end
 
-  create_table "images_markdown_contents", id: false, force: true do |t|
-    t.integer "markdown_content_id", null: false
-    t.integer "image_id",            null: false
-  end
-
-  add_index "images_markdown_contents", ["image_id", "markdown_content_id"], name: "image_md_index", using: :btree
-  add_index "images_markdown_contents", ["markdown_content_id", "image_id"], name: "md_image_index", using: :btree
-
   create_table "issues", force: true do |t|
     t.integer  "year"
     t.string   "slug"
@@ -106,6 +101,14 @@ ActiveRecord::Schema.define(version: 20140311164734) do
   end
 
   add_index "issues", ["slug"], name: "index_issues_on_slug", unique: true, using: :btree
+
+  create_table "markdown_content_images", id: false, force: true do |t|
+    t.integer "markdown_content_id", null: false
+    t.integer "image_id",            null: false
+  end
+
+  add_index "markdown_content_images", ["image_id", "markdown_content_id"], name: "image_md_index", using: :btree
+  add_index "markdown_content_images", ["markdown_content_id", "image_id"], name: "md_image_index", using: :btree
 
   create_table "markdown_contents", force: true do |t|
     t.text     "data"
