508a2339b0093d14904dd3697082a908814716e0
Andy Wetherill <awetheri@nd.edu>
2014-03-19 11:09:01 -0400

    Admin 2 panel display

--- db/schema.rb
+++ db/schema.rb
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20140318171441) do
   create_table "essay_styles", force: true do |t|
     t.string   "slug"
     t.text     "data"
+    t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -68,6 +69,8 @@ ActiveRecord::Schema.define(version: 20140318171441) do
   create_table "essays", force: true do |t|
     t.integer  "issue_id"
     t.string   "slug"
+    t.string   "title"
+    t.integer  "placement"
     t.text     "data",           limit: 2147483647
     t.datetime "created_at"
     t.datetime "updated_at"
