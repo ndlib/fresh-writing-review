e51a9b5c21fd36c0aa7d7d65c58f64836d628b9d
Robert Fox <robertjonfox@gmail.com>
2014-03-20 14:17:17 -0400

    Add cleanup for markdown interface, add alt text, caption and credit to image dialog

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,7 +2,7 @@ class Image < ActiveRecord::Base
   
   has_many :markdown_contents_images
 
-  has_attached_file :image, { :path => ":rails_root/public/system/:class/:id/:style/:filename" }
+  has_attached_file :image, { :path => ":rails_root/public/system/:class/:id/:style/:filename", :url => "/system/:class/:id/:style/:filename" }
   
   validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png)
   validates :image, presence: true
