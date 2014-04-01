345b661207d748a3d53f6e13d2ad760dc7c7fa14
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2014-04-01 11:53:45 -0400

    tested and updated the issue list and issue detail.

--- app/view_models/admin/issue_detail.rb
+++ app/view_models/admin/issue_detail.rb
@@ -18,7 +18,23 @@ class Admin::IssueDetail
 
 
   def edit_button
-    helpers.link_to('Edit', routes.edit_admin_issue_path(issue), class: 'btn btn-primary')
+    helpers.link_to('Edit', routes.edit_admin_issue_path(issue.id), class: 'btn btn-primary')
+  end
+
+
+  def issue_status
+    if issue.published?
+      "<span class=\"glyphicon glyphicon-eye-open\"></span> Published"
+    else
+      "<span class=\"glyphicon glyphicon-eye-close\"></span> Unpublished"
+    end
+  end
+
+
+  def file_icon
+    if issue.is_pdf?
+      "<span class=\"pdf glyphicon glyphicon-file\" title=\"PDF Only\"></span>"
+    end
   end
 
 
--- spec/view_models/admin/issue_detail_spec.rb
+++ spec/view_models/admin/issue_detail_spec.rb
@@ -2,7 +2,41 @@ require 'spec_helper'
 
 describe Admin::IssueDetail do
 
+  let(:issue) { double(Issue, id: 1, title: 'title', published?: true, is_pdf?: true )}
+  subject { Admin::IssueDetail.new(issue)}
 
-  it ""
+  it "has a title" do
+    expect(subject.title).to eq("title")
+  end
 
+
+  it "displays an edit button for the issue" do
+    expect(subject.edit_button).to eq("<a class=\"btn btn-primary\" href=\"/admin/issues/1/edit\">Edit</a>")
+  end
+
+
+  describe "#published" do
+
+    it "shows the published status when it is published" do
+      expect(subject.issue_status).to eq("<span class=\"glyphicon glyphicon-eye-open\"></span> Published")
+    end
+
+    it "shows the unpublished status when it is unpublished" do
+      issue.stub(:published?).and_return(false)
+      expect(subject.issue_status).to eq("<span class=\"glyphicon glyphicon-eye-close\"></span> Unpublished")
+    end
+  end
+
+
+  describe "#file_icon" do
+
+    it "shows the file icon when there the issue is a file type" do
+      expect(subject.file_icon).to eq("<span class=\"pdf glyphicon glyphicon-file\" title=\"PDF Only\"></span>")
+    end
+
+    it "shows nothing when the issues is not a file type" do
+      issue.stub(:is_pdf?).and_return(false)
+      expect(subject.file_icon).to eq(nil)
+    end
+  end
 end
