60bae979be5d71cf8f2a9ef677f137b4be6c3796
Jaron Kennel <jaronkk@gmail.com>
2014-04-01 13:33:20 -0400

    Editor listing and delete links

--- app/controllers/admin/editors_controller.rb
+++ app/controllers/admin/editors_controller.rb
@@ -6,4 +6,23 @@ class Admin::EditorsController < ApplicationController
     @editors = EditorQuery.all
   end
 
+  def new
+    @editor = Editor.new
+  end
+
+  def create
+    @editor = Editor.new(params.require(:editor).permit(:username))
+    if @editor.save
+      redirect_to admin_editors_path
+    else
+      render action: :new
+    end
+  end
+
+  def destroy
+    @editor = EditorQuery.find(params[:id])
+    @editor.destroy
+    redirect_to admin_editors_path
+  end
+
 end
--- app/models/editor.rb
+++ app/models/editor.rb
@@ -2,4 +2,5 @@ class Editor < ActiveRecord::Base
   belongs_to :user, foreign_key: :username, primary_key: :username
 
   validates_presence_of :username
+  validates_uniqueness_of :username
 end
