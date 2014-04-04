05f8ad782c628a72009ca92f5bc5f9067ce0b048
Jaron Kennel <jaronkk@gmail.com>
2014-04-04 15:20:39 -0400

    Basic Page form

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -4,9 +4,18 @@ class Admin::PagesController < AdminController
   end
 
   def edit
+    @page = PageQuery.find(params[:id])
   end
 
   def update
+    @page = PageQuery.find(params[:id])
+
+    if @page.update_attributes(params.require(:page).permit(:title))
+      flash[:success] = "Page \"#{@page.title}\" updated"
+      redirect_to admin_pages_path()
+    else
+      render action: :edit
+    end
   end
 
 end
