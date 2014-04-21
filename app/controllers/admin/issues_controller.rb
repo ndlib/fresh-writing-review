130e0c8948199d9ab14b699fa0ced19621d22a16
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2014-04-21 11:23:50 -0400

    fix the issue where the admin flash uses issue instead of volume.

--- app/controllers/admin/issues_controller.rb
+++ app/controllers/admin/issues_controller.rb
@@ -25,7 +25,7 @@ class Admin::IssuesController < AdminController
       flash[:success] = 'Issue Created'
       redirect_to admin_issue_path(@form.issue)
     else
-      flash.now[:error] = 'Unable to create the issue.  Please review the error messages below and correct the form.'
+      flash.now[:error] = 'Unable to create the volume.  Please review the error messages below and correct the form.'
       render :new
     end
   end
@@ -43,7 +43,7 @@ class Admin::IssuesController < AdminController
       flash[:success] = 'Issue Updated'
       redirect_to admin_issue_path(@form.issue)
     else
-      flash.new[:error] = 'Unable to update the issue.  Please review the error messages below and correct the form.'
+      flash.new[:error] = 'Unable to update the volume.  Please review the error messages below and correct the form.'
       render :new
     end
   end
