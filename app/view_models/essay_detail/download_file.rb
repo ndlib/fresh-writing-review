ed138e3f74438f9f3e0c628a29c3d051a26b240c
Jaron Kennel <jaronkk@gmail.com>
2014-08-28 09:23:59 -0400

    Added draper, updated download_file to use draper

--- app/view_models/essay_detail/download_file.rb
+++ app/view_models/essay_detail/download_file.rb
@@ -1,40 +1,38 @@
-class EssayDetail::DownloadFile
-
-  include RailsHelpers
+class EssayDetail::DownloadFile < Draper::Decorator
 
   def self.render(essay)
     self.new(essay).render
   end
 
 
-  def initialize(essay)
-    @essay = essay
+  def essay
+    object
   end
 
 
   def has_files?
-    @essay.attached_file.present?
+    essay.attached_file.present?
   end
 
 
   def title
-    @essay.attached_file.title
+    essay.attached_file.title
   end
 
 
   def body
-    @essay.attached_file.body
+    essay.attached_file.body
   end
 
 
   def url
-    @essay.attached_file.file.url
+    essay.attached_file.file.url
   end
 
 
   def render
     if has_files?
-      render_to_string '/essays/attached_files', { object: self}
+      h.render partial: '/essays/attached_files', locals: { object: self}
     end
   end
 
