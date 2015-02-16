205b173019e24632c7b5f3ba9a0c6d172a47d8a6
Dan Wolfe <dwolfe2@nd.edu>
2015-02-16 13:51:00 -0500

    http:// ==> https://

--- app/assets/javascripts/together-settings.js
+++ app/assets/javascripts/together-settings.js
@@ -1,6 +1,6 @@
 var TogetherJSConfig_siteName = "Beehive";
 var TogetherJSConfig_toolName = "Collaborate";
-var TogetherJSConfig_hubBase = "http://freestyle.library.nd.edu";
+var TogetherJSConfig_hubBase = "https://freestyle.library.nd.edu";
 var TogetherJSConfig_cloneClicks = true;
 var TogetherJSConfig_suppressJoinConfirmation = true;
 
