7a988fe85c2c3c1bd96565cff9e50b085b40fea3
Dan Wolfe <dwolfe2@nd.edu>
2015-02-16 14:36:52 -0500

    undo

--- app/assets/javascripts/together-settings.js
+++ app/assets/javascripts/together-settings.js
@@ -1,6 +1,6 @@
 var TogetherJSConfig_siteName = "Fresh Writing";
 var TogetherJSConfig_toolName = "Collaborate";
-//var TogetherJSConfig_hubBase = "https://freestyle.library.nd.edu:8443";
+var TogetherJSConfig_hubBase = "https://freestyle.library.nd.edu:8443";
 var TogetherJSConfig_cloneClicks = true;
 var TogetherJSConfig_suppressJoinConfirmation = true;
 
