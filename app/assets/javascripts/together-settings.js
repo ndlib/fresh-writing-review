5146311556c43d8ff7735108a581811a1906c2ca
Dan Wolfe <dwolfe2@nd.edu>
2015-02-16 15:47:05 -0500

    user default server

--- app/assets/javascripts/together-settings.js
+++ app/assets/javascripts/together-settings.js
@@ -1,8 +1,8 @@
 var TogetherJSConfig_siteName = "Fresh Writing";
 var TogetherJSConfig_toolName = "Collaborate";
-var TogetherJSConfig_hubBase = "https://freestyle.library.nd.edu:8443";
-var TogetherJSConfig_cloneClicks = true;
-var TogetherJSConfig_suppressJoinConfirmation = true;
+//var TogetherJSConfig_hubBase = "https://freestyle.library.nd.edu:8443";
+//var TogetherJSConfig_cloneClicks = true;
+//var TogetherJSConfig_suppressJoinConfirmation = true;
 
 $('#btnTogether').click(function(event) {
   event.preventDefault();
