bcc557d93b6ef24ca6b7a5e5ea8aefa2a50fae1d
Tom Hanstra <hanstra@nd.edu>
2019-08-21 11:52:03 -0400

    Spelling out freshwriting in prep environment

--- config/environments/prep.rb
+++ config/environments/prep.rb
@@ -65,7 +65,7 @@ FreshWriting::Application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { :host => 'fw-prep.library.nd.edu' }
+  config.action_mailer.default_url_options = { :host => 'freshwriting-prep.library.nd.edu' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
